0956ce45e7368c1a6998b5d765ffb0f1c0b81f42
ebichan38 <>
2021-06-16 13:50:45 +0900

    remove ecma import (does not work)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule } from '@angular/material/dialog';
-import { ModalModule } from './modal/modal.module';
 
 @NgModule({
   declarations: [
